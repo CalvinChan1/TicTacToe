7bd604e100f73ae75294120379f889ca31669453
Calvin Chan <calvin_chan137@outlook.com>
2015-11-07 13:20:58 -0500

    added button action

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -13,10 +13,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    // MARK: Actions
+    
+    @IBAction func setDefaultLabelText(sender: UIButton) {
+        mealNameLabel.text = "wassup nigga"
+    }
 }
 
+    
